a35fadfcf6e339cbf2c60d6e034785feaf50647c
tauon <tauon@qubytes.de>
2018-04-24 00:06:03 +0200

    test 18

--- src/foo/testclass.groovy
+++ src/foo/testclass.groovy
@@ -2,7 +2,7 @@ package foo
 
 class testclass {
    static def DisplayName(script) {
-        pyscript = script.libraryResource "python/test.py"
+        def pyscript = script.libraryResource "python/test.py"
         script.writeFile file: 'test.py', text: pyscript
         script.sh "python text.py"
    } 
